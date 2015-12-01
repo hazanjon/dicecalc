0ee2c0a451130c5c4547e24a0583a2d270704b5c
Jon Hazan <jon@hazan.me>
2015-12-01 20:27:18 +0000

    Basic Yo Angular Setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name diceCalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the diceCalcApp
+ */
+angular.module('diceCalcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name diceCalcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the diceCalcApp
+ */
+angular.module('diceCalcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
